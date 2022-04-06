1e84bd9ea439d5c088356517875fa6ade36db0e2
Andreas Notokuusmo <andreasnotokusumo23@gmail.com>
2022-04-06 15:20:05 +0700

    :sparkles: Player UI Refine

--- lib/presentation/screens/player/player_page.dart
+++ lib/presentation/screens/player/player_page.dart
@@ -1,4 +1,9 @@
 import 'package:flutter/cupertino.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:leeplay/config/media_query.dart';
+import 'package:leeplay/config/theme.dart';
+import 'package:leeplay/domain/theme/cubit/theme_cubit.dart';
+import 'package:marquee/marquee.dart';
 
 class PlayerPage extends StatefulWidget {
   const PlayerPage({Key? key}) : super(key: key);
@@ -12,20 +17,178 @@ class _PlayerPageState extends State<PlayerPage> {
   Widget build(BuildContext context) {
     return CupertinoPageScaffold(
       navigationBar: CupertinoNavigationBar(
-        leading: const Icon(CupertinoIcons.back),
+        leading: CupertinoButton(
+          minSize: 0,
+          padding: EdgeInsets.zero,
+          onPressed: () {
+            Navigator.pop(context);
+          },
+          child: const Icon(CupertinoIcons.back, size: 22),
+        ),
         trailing: Row(
-          children: const [
+          mainAxisSize: MainAxisSize.min,
+          children: [
             Padding(
-              padding: EdgeInsets.only(right: 12.0),
-              child: Icon(CupertinoIcons.heart),
+              padding: const EdgeInsets.only(right: 20.0, top: 2.0),
+              child: CupertinoButton(
+                minSize: 0,
+                padding: EdgeInsets.zero,
+                onPressed: () {
+                  Navigator.pop(context);
+                },
+                child: const Icon(CupertinoIcons.heart, size: 24),
+              ),
             ),
-            Icon(CupertinoIcons.share)
+            CupertinoButton(
+              minSize: 0,
+              padding: EdgeInsets.zero,
+              onPressed: () {
+                Navigator.pop(context);
+              },
+              child: const Icon(CupertinoIcons.share, size: 22),
+            ),
           ],
         ),
       ),
-      child: Column(
-        children: const [
-          
+      child: ListView(
+        children: [
+          AspectRatio(
+            aspectRatio: 16 / 9,
+            child: Image.asset("assets/sample_thumb.png",
+                fit: BoxFit.cover,
+                width: MQuery.width(1, context),
+                alignment: Alignment.center),
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 24.0, vertical: 16.0),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Expanded(
+                  flex: 6,
+                  child: Padding(
+                    padding: const EdgeInsets.only(right: 24.0),
+                    child: SizedBox(
+                      height: 36,
+                      child: Marquee(
+                        text: "우리 나중에 파리 여행 가면 에펠탑 보면서 같이 듣자",
+                        style: AppText.title3,
+                        scrollAxis: Axis.horizontal,
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        blankSpace: 20.0,
+                        velocity: 20.0,
+                        startPadding: 10.0,
+                      ),
+                    ),
+                  ),
+                ),
+                SizedBox(
+                  height: 36,
+                  child: Center(
+                    child: CupertinoButton(
+                      minSize: 0,
+                      padding: EdgeInsets.zero,
+                      onPressed: () {
+                        Navigator.pop(context);
+                      },
+                      child: const Icon(CupertinoIcons.info, size: 24),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+          Column(
+            children: [
+              Container(
+                padding: const EdgeInsets.symmetric(horizontal: 12.0),
+                width: double.infinity,
+                child: CupertinoSlider(
+                  value: 70,
+                  max: 100,
+                  min: 0,
+                  onChanged: (double newValue) {},
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 24.0),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    Text(
+                      "40:42",
+                      style: AppText.footnote.copyWith(fontWeight: FontWeight.w600),
+                    ),
+                    Text(
+                      "58:12",
+                      style: AppText.footnote.copyWith(fontWeight: FontWeight.w600),
+                    ),
+                  ],
+                ),
+              )
+            ],
+          ),
+          Padding(
+            padding: const EdgeInsets.only(top: 16.0),
+            child: Center(
+              child: BlocBuilder<ThemeCubit, ThemeState>(
+                builder: (context, state) {
+                  return Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      CupertinoButton(
+                        minSize: 0,
+                        padding: EdgeInsets.zero,
+                        onPressed: () {
+                          Navigator.pop(context);
+                        },
+                        child: Icon(
+                          CupertinoIcons.backward_fill,
+                          size: 32,
+                          color: state is LightTheme
+                              ? CupertinoColors.black
+                              : CupertinoColors.white,
+                        ),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.symmetric(horizontal: 28.0),
+                        child: CupertinoButton(
+                          minSize: 0,
+                          padding: EdgeInsets.zero,
+                          onPressed: () {
+                            Navigator.pop(context);
+                          },
+                          child: Icon(
+                            CupertinoIcons.play_circle_fill,
+                            size: 72,
+                            color: state is LightTheme
+                                ? CupertinoColors.black
+                                : CupertinoColors.white,
+                          ),
+                        ),
+                      ),
+                      CupertinoButton(
+                        minSize: 0,
+                        padding: EdgeInsets.zero,
+                        onPressed: () {
+                          Navigator.pop(context);
+                        },
+                        child: Icon(
+                          CupertinoIcons.forward_fill,
+                          size: 32,
+                          color: state is LightTheme
+                              ? CupertinoColors.black
+                              : CupertinoColors.white,
+                        ),
+                      ),
+                    ],
+                  );
+                },
+              ),
+            ),
+          )
         ],
       ),
     );
